032a314761fe2324bac6ffc11135739f7bb73d0a
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-07-12 12:34:49 -0300

    Criando modelos e templates

--- Olimpiadas/Competicao/views.py
+++ Olimpiadas/Competicao/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from .models import Competicao, Atleta
 
-def index(request):
-    return render(request, 'index.html')
+
+class IndexView(generic.ListView):
+    template_name: str = 'index.html'
+    
+    def index(self, request):
+        return render(request, 'index.html')
+    
+    def get_queryset(self):
+        return Competicao.objects.filter(data_inicio__lte=timezone.now()).order_by('data_inicio')
+
+class CadstroView(generic.CreateView):
+    model = Competicao
+    fields = ['nome', 'data_inicio', 'data_fim', 'descricao']
+    template_name = 'cadastro.html'
+    
+    def cadastro(self, request):
+        return render(request, 'cadastro.html')
+    
+    def post(self, request):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
+    def get_success_url(self):
+        return reverse('index')
