66b84eb8cb7910c3bdd22853132962b5b728c7f7
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-07-18 13:23:05 -0300

    Atualizando tabela de resultado

--- nova_competicao/urls.py
+++ nova_competicao/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.criar_competicao, name='criar_competicao'),
+    path('ranking/', views.ranking, name='ranking'),
 ]
--- nova_competicao/views.py
+++ nova_competicao/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render
 from .forms import ClientForms
+from .models import Competicao
 
 def criar_competicao(request):
     if request.method == 'GET':
@@ -18,3 +19,7 @@ def criar_competicao(request):
             'form': form
         }
         return render(request, 'criar.html', context=context)
+
+def ranking(request):
+    context = Competicao.objects.all()
+    return render(request, 'ranking.html', {'context':context})
